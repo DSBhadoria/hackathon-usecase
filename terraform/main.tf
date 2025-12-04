69ce48616d66b6c0c4374f191160d74589822e71
DSBhadoria <singhbhadoria96@yahoo.in>
2025-12-04 09:30:13 +0530

    resource azurerm_kubernetes_cluster added

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,3 +21,57 @@ resource "azurerm_resource_group" "rg_azure" {
   location = var.resource_group_location
   name     = random_pet.rg_name.id
 }
+
+resource "azurerm_network_security_group" "nat" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.rg_azure.location
+  resource_group_name = azurerm_resource_group.rg_azure.name
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "example-network"
+  location            = azurerm_resource_group.rg_azure.location
+  resource_group_name = azurerm_resource_group.rg_azure.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "public-subnet"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "private-subnet"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.nat.id
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "aks-cluster"
+  location            = azurerm_resource_group.rg_azure.location
+  resource_group_name = azurerm_resource_group.rg_azure.name
+  dns_prefix          = "aksckuster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_resource_group.rg_azure.id
+  # scope                = azurerm_container_registry.acr.id
+}
